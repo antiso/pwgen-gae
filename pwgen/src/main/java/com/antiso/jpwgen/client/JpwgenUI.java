6af7a0423ac0f79095313d7d92f42bd892cbff26
Vladimir Sosnin <antiso@gmail.com>
2013-05-02 12:48:10 +0400

    Initial working version

---
+++ pwgen/src/main/java/com/antiso/jpwgen/client/JpwgenUI.java
@@ -0,0 +1,14 @@
+package com.antiso.jpwgen.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class JpwgenUI implements EntryPoint {
+
+	public void onModuleLoad() {
+		RootPanel.get().add(new Label("Hello!"));
+
+	}
+
+}
